b77ad1b6427eaf910d0d83f6c46e3b8be212932a
Lam Nguyen <lnguyen@nephronpharm.com>
2021-06-08 14:58:07 -0400

    minichange to test updates

--- printer.go
+++ printer.go
@@ -4,6 +4,11 @@ import (
     "fmt"
 )
 
+// PrintHelloWorld() acts as a basic test to see that HelloWorld is imported correctly
 func PrintHelloWorld() {
-    fmt.Println("Hello World from the goshared repository!")
+    fmt.Println("Hello World from the lamn18/goshared repository!")
+}
+
+func PrintSlice() {
+
 }
